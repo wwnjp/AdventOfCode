e85297bb6930c52b097fd98705f1f29fbc2eb350
Josh Drumm <joshdrumm@gmail.com>
2020-12-11 14:51:55 -0500

    More minify 10

--- 11.js
+++ 11.js
@@ -4,36 +4,36 @@ const is = (c,y,x) => map[y] && map[y][x] && map[y][x] === c;
 
 const look = (y,x,dx,dy) => {
 	let xx=x,yy=y;
-	do { xx += dx; yy += dy; } while (is('.',yy,xx));
+	do {xx+=dx;yy+=dy;} while (is('.',yy,xx));
 	return is('#',yy,xx);
 };
 
 const yarp = (y,x) => 
 	[[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]]
-		.map(s => look(y,x,s[0],s[1]))
-		.filter(s => s).length;
+		.map(s=>look(y,x,s[0],s[1]))
+		.filter(s=>s).length;
 
 const changes = (t) => 
 	map.map((line,y) => 
 		line.map((l,x) => 
-			(is('L',y,x) && !yarp(y,x)) ? '#' :
-			(is('#',y,x) && yarp(y,x) > t) ? 'L' :
+			(is('L',y,x) && !yarp(y,x))?'#':
+			(is('#',y,x) && yarp(y,x)>t)?'L':
 			l));
 
-const up = t => {
-	let _m = changes(t);
+const sameee = _ => JSON.stringify(_) === JSON.stringify(map);
+const ct = m => m.map(_ => _.join('')).join('').split('').filter(_ => _ === '#').length;
 
-	if (JSON.stringify(_m) === (JSON.stringify(map))) {
-		return false;
-	}
-	else {
-		map = _m;
-		return true;
-	}
+const up = t => {
+	const _m = changes(t);
+
+	if (sameee(_m)) throw ct(_m);
+	return map = _m;
 }
 
-const ct = () => map.map(_ => _.join('')).join('').split('').filter(_ => _ === '#').length;
+try {
+	while(up(4));
+}
+catch (e) {
+	console.log('PART 2:', e);
+}
 
-while(up(4));
-
-console.log('PART 2:', ct());
