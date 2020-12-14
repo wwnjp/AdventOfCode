fd414ab39e169335d47edc274c2219207df719ff
Josh Drumm <joshdrumm@gmail.com>
2020-12-14 13:21:17 -0500

    Day 14 annotation

--- 14.1.js
+++ 14.1.js
@@ -7,13 +7,14 @@ let _mem = [];
 const getAddr = memS => parseInt(memS.replace(/[^\d]/g, ''));
 
 const applyMask = v => {
+	// Convert the value to 36bit string
 	const vv = parseInt(v).toString(2).padStart(36, '0');
 	
-	return parseInt(_mask
-		.split('')
-		.map(b => parseInt(b))
-		.map((b, i) => isNaN(b) ? vv[i] : b)
-		.join(''), 2);
+	return parseInt(_mask					// Take the mask
+		.split('')							// Look at every bit
+		.map(b => parseInt(b))				// Run it through a parseInt(), if it's 1, or 0, return it, X will throw NaN
+		.map((b, i) => isNaN(b) ? vv[i] : b)// for each (b)it, if it's NaN return the bit in the slot vv[i], otherwise, b 
+		.join(''), 2);						// smash it back together, convert to number, \o/
 }
 
 
