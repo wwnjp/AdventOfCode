8f4d81bf138ccd17d2bfb8af6db91c46f91564e4
Josh Drumm <joshdrumm@gmail.com>
2020-12-14 19:30:06 -0500

    Day 14-2 annotations

--- 14.2.js
+++ 14.2.js
@@ -13,6 +13,10 @@ const applyVals = (addr, v) => {
 	let xplace = 0;
 	let addrBits = addr.toString(2).padStart(36, '0').split('');
 
+	// For every element we want to set mem, 
+	// we are going to set however many Xs are in the mask
+	// Iterate over each num, build the binary representation, (p)
+	// place them in the X places (xplace).
 	for (let i = 0; i < Math.pow(2, numAddrs); i++) {
 		xplace = 0;
 		p = i.toString(2).padStart(numAddrs, '0').split('');
@@ -23,6 +27,9 @@ const applyVals = (addr, v) => {
 			if (b === 'X') { return p[xplace++]; }
 		}).map(b => parseInt(b)).join(''), 2);
 
+		// since this array will get larger than
+		// 2^32 elements, using a string key hash 
+		// will guarantee we don't lose any elements
 		_mem[memAddr.toString()] = v;
 	}
 }
@@ -40,10 +47,6 @@ set.forEach(l => {
 });
 
 let sum = 0;
-for (i in _mem) {
-	if (_mem[i]) {
-		sum += _mem[i];
-	}
-}
+for (i in _mem) sum += _mem[i];
 
 console.log('PART 2:', sum);
