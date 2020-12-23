6e48d3a7415a3e1f52d07e5305af831ce8a26d18
Josh Drumm <joshdrumm@gmail.com>
2020-12-22 20:38:09 -0500

    Day 22 more opitmizations

--- 22.js
+++ 22.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const input = fs.readFileSync('./inputs/22.txt', 'utf-8').trim().split('\n\n');
-
 const players = input.map(i => i.split(':\n')[1].split('\n').map(Number));
 
 while (players.every(p => p.length)) {
 	const plays = players.map(p => p.shift());
-	const winner = plays.indexOf(Math.max(...plays));
-	players[winner].push(...plays.sort((a, b) => a > b ? -1 : 1));
+	players[plays.indexOf(Math.max(...plays))].push(...plays.sort((a, b) => a > b ? -1 : 1));
 }
 
 console.log('PART 1', players.find(p => p.length).map((c, i, a) => c * (a.length - i)).reduce((a, b) => a + b, 0));
